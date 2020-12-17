e728c6a20e592ee3c28042b716b071ed9234af00
Cesar <cesar@localhost.localdomain>
2020-12-16 22:22:29 -0500

    commit de materialice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavegacionComponent } from '../../../App-Angular/src/app/components/navegacion/navegacion.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PresentacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
